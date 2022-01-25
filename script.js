c186a653683914aa192d320a392098b9b817b05d
ilya <ilakolasnikov1@gmail.com>
2022-01-26 00:29:48 +0300

    novaya funktsional'nost'

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict"
 
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели? ", "");
-let lastMovie, grade;
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели? ", "");
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -11,10 +10,26 @@ const personalMovieDB = {
   private: false
 }
 
+if (personalMovieDB.count <= 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count > 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
+
 for (let i = 0; i < 2; i++) {
-  lastMovie = prompt("Один из последниз просмотренных фильмов? ", "");
-  grade = prompt("Насколько вы его оцените? ", "");
-  personalMovieDB.movies[lastMovie] = grade;
+  let lastMovie = prompt("Один из последниз просмотренных фильмов? ", ""),
+      grade     = prompt("Насколько вы его оцените? ", "");
+  if (lastMovie != "" && grade != "" && lastMovie != null && grade != null && lastMovie.length < 50 && grade.length < 50) {
+    personalMovieDB.movies[lastMovie] = grade;
+    console.log("Done");
+  } else {
+    console.log("error");
+    i--;
+  }
 }
 
 console.log(personalMovieDB);
